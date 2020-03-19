c44dccd3c65858c513066c2adc0d2f81619fa239
Balaji Varadharajan <vbalaji215@gmail.com>
2020-03-19 16:29:40 -0400

    Beer JPA Entity is created. closes #10

---
+++ src/main/java/com/bpjj/beer/service/domain/Beer.java
@@ -0,0 +1,49 @@
+package com.bpjj.beer.service.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * vbala created on 3/19/2020
+ * Inside the package - com.bpjj.beer.service.domain
+ **/
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name ="UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID beerId;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    @Column(updatable = false)
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer minQtyOnHand;
+    private Integer quantityToBrew;
+
+}

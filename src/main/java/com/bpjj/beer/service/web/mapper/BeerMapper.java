d7cd64f3b0091f0f45a638bb711fdf93490b9803
Balaji Varadharajan <vbalaji215@gmail.com>
2020-03-21 14:33:53 -0400

    Added Mapstruct for type conversion. closes #19

---
+++ src/main/java/com/bpjj/beer/service/web/mapper/BeerMapper.java
@@ -0,0 +1,16 @@
+package com.bpjj.beer.service.web.mapper;
+
+import com.bpjj.beer.service.domain.Beer;
+import com.bpjj.beer.service.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * vbala created on 3/21/2020
+ * Inside the package - com.bpjj.beer.service.web.mapper
+ **/
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto beerDto);
+}

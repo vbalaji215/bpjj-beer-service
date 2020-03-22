1f5bbcfca90796f878564dfbf8f8c12f631dfab3
Balaji Varadharajan <vbalaji215@gmail.com>
2020-03-22 19:04:00 -0400

    Add Standard UPCs, make UPC String. closes #28

--- src/main/java/com/bpjj/beer/service/bootstrap/BeerServiceDataInitializer.java
+++ src/main/java/com/bpjj/beer/service/bootstrap/BeerServiceDataInitializer.java
@@ -17,6 +17,10 @@ import java.math.BigDecimal;
 @Slf4j
 public class BeerServiceDataInitializer implements CommandLineRunner {
 
+    public static final String BEER_1_UPC = "032345340043";
+    public static final String BEER_2_UPC = "032345340044";
+    public static final String BEER_3_UPC = "032345340045";
+
     private final BeerRepository beerRepository;
 
     public BeerServiceDataInitializer(BeerRepository beerRepository) {
@@ -34,13 +38,19 @@ public class BeerServiceDataInitializer implements CommandLineRunner {
                     "IPA",
                     12,
                     200,
-                    337010001001L,
+                    BEER_1_UPC,
                     new BigDecimal("12.95"));
             saveBeer("Galaxy Cat",
                     "PALE_ALE",
                     12,
                     200,
-                    337010001002L,
+                    BEER_2_UPC,
+                    new BigDecimal("11.95"));
+            saveBeer("No Hammers on the Bar",
+                    "PALE_ALE",
+                    12,
+                    200,
+                    BEER_3_UPC,
                     new BigDecimal("11.95"));
         }
         log.debug("Loaded Beers:" + beerRepository.count());
@@ -50,7 +60,7 @@ public class BeerServiceDataInitializer implements CommandLineRunner {
                           String beerStyle,
                           Integer minQtyOnHand,
                           Integer quantityToBrew,
-                          Long upc,
+                          String upc,
                           BigDecimal price){
         beerRepository.save(Beer.builder()
                 .beerName(beerName)
